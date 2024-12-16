36ab84927d9fc2517cb46d97e3b346993922ab36
Dazhong Xia <dazhong.xia@catalyst.coop>
2024-12-16 18:09:47 -0500

    Clean up JS a bit, start making Python schema-aware

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,119 @@
 import * as duckdb from '@duckdb/duckdb-wasm';
 import * as arrow from 'apache-arrow';
-import perspective from "@finos/perspective";
+import perspective, { Table } from "@finos/perspective";
 import "@finos/perspective-viewer";
 import "@finos/perspective-viewer-datagrid";
 import "@finos/perspective-viewer-d3fc";
 
 import "@finos/perspective-viewer/dist/css/pro-dark.css";
 import "./index.css";
-import { PerspectiveViewerElementExt } from '@finos/perspective-viewer';
 import { PerspectiveViewerElement } from '@finos/perspective-viewer/dist/pkg/perspective-viewer';
 
-const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
 
-// Select a bundle based on browser checks
-const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+async function initializeDuckDB(): Promise<duckdb.AsyncDuckDB> {
+  const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
 
-const worker_url = URL.createObjectURL(
-  new Blob([`importScripts("${bundle.mainWorker!}");`], {type: 'text/javascript'})
-);
+  // Select a bundle based on browser checks
+  const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
 
-// Instantiate the asynchronous version of DuckDB-wasm
-const worker = new Worker(worker_url);
-const logger = new duckdb.ConsoleLogger();
-const db = new duckdb.AsyncDuckDB(logger, worker);
-await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
-URL.revokeObjectURL(worker_url);
+  const worker_url = URL.createObjectURL(
+    new Blob([`importScripts("${bundle.mainWorker!}");`], { type: 'text/javascript' })
+  );
 
-const baseUrl = "https://s3.us-west-2.amazonaws.com/pudl.catalyst.coop/stable/"
-const maybeTableName = document.getElementById("table-name");
-const tableName = maybeTableName ? maybeTableName.innerText : "";
-const filename = `${tableName}.parquet`;
-const url = `${baseUrl}${filename}`;
-await db.registerFileURL(filename, url, duckdb.DuckDBDataProtocol.HTTP, false);
+  // Instantiate the asynchronous version of DuckDB-wasm
+  const worker = new Worker(worker_url);
+  const logger = new duckdb.ConsoleLogger();
+  const db = new duckdb.AsyncDuckDB(logger, worker);
+  await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+  URL.revokeObjectURL(worker_url);
+  return db;
+}
 
-async function getQuery(
-  {tableName, filter, forDownload = false}
-  : { tableName: string, filter: Array<Array<string>>, forDownload?: boolean }
+function getTableNameFromHeader() {
+  const maybeTableName = document.getElementById("table-name");
+  return maybeTableName ? maybeTableName.innerText : "";
+}
+
+async function addTableToDuckDB(db: duckdb.AsyncDuckDB, tableName: string) {
+  const baseUrl = "https://s3.us-west-2.amazonaws.com/pudl.catalyst.coop/stable/"
+  const filename = `${tableName}.parquet`;
+  const url = `${baseUrl}${filename}`;
+  await db.registerFileURL(filename, url, duckdb.DuckDBDataProtocol.HTTP, false);
+}
+
+async function getDuckDBQuery(
+  { tableName, filter, forDownload = false }
+    : { tableName: string, filter: Array<Array<string>>, forDownload?: boolean }
 ) {
   const params = new URLSearchParams(
-      {tableName, filter: JSON.stringify(filter), forDownload: JSON.stringify(forDownload)}
+    { tableName, filter: JSON.stringify(filter), forDownload: JSON.stringify(forDownload) }
   );
   const resp = await fetch("/api/duckdb?" + params);
   const query = await resp.text();
   return query
 }
 
-async function getTableDataForViewer(viewer: PerspectiveViewerElement, forDownload: boolean=false): Promise<arrow.Table> {
-  const {filter: rawFilter} = await viewer.save();
+async function getTableDataForViewer(
+  tableName: string, viewer: PerspectiveViewerElement, c: duckdb.AsyncDuckDBConnection, forDownload: boolean = false
+): Promise<arrow.Table> {
+  const { filter: rawFilter } = await viewer.save();
   const filter = rawFilter.filter((e: Array<string>) => e[2]);
 
   const filterVals = filter.map((e: Array<string>) => e[2]);
-  const query = await getQuery({tableName, filter, forDownload: forDownload});
+  const query = await getDuckDBQuery({ tableName, filter, forDownload: forDownload });
   const stmt = await c.prepare(query);
   console.log("query ", query);
   console.log("filtervals ", filterVals);
-  const newData =  await stmt.query(...filterVals);
+  const newData = await stmt.query(...filterVals);
   console.log(`got ${newData.numRows} rows of data`);
   return newData;
 }
 
-const pworker = await perspective.worker();
+function arrowToCsv(table: arrow.Table): Blob {
+  const csv = table.toArray()
+    .map(row => Object.values(row).join(','))
+    .join('\n');
 
+  const headers = table.schema.fields.map(f => f.name).join(',');
+  const csvWithHeaders = headers + '\n' + csv;
+
+  return new Blob([csvWithHeaders], { type: 'text/csv' });
+}
+
+function downloadBlob(blob: Blob) {
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = "mydata.csv";
+  link.click();
+  URL.revokeObjectURL(url);
+}
+
+const db = await initializeDuckDB();
 const c = await db.connect();
+const tableName = getTableNameFromHeader();
+await addTableToDuckDB(db, tableName);
+
 const viewer = document.getElementsByTagName("perspective-viewer")[0];
-const tableData =  await getTableDataForViewer(viewer);
+
+// if there's no schema yet, what do we do?
+// could rely on there being no filter.
+// could also pre-fetch the schema.
+const tableData = await getTableDataForViewer(tableName, viewer, c);
+
+const pworker = await perspective.worker();
 const table = await pworker.table(arrow.tableToIPC(tableData));
+
+
 viewer.load(table);
+
+const schema = await table.schema();
+console.log(schema)
 
 let timeout: number;
 viewer.addEventListener("perspective-config-update", async (event) => {
-  // TODO: pass the schema to Python so we can figure out the right DuckDB query.
-  const schema = await table.schema();
-
   const runQuery = async () => {
-    const newData =  await getTableDataForViewer(viewer);
+    const newData = await getTableDataForViewer(tableName, viewer, c);
     table.replace(arrow.tableToIPC(newData));
   }
 
@@ -84,20 +124,8 @@ viewer.addEventListener("perspective-config-update", async (event) => {
 
 const downloader = document.getElementById("csv-download") as HTMLButtonElement;
 downloader.onclick = async () => {
-  const newData =  await getTableDataForViewer(viewer, true);
-
-  const csv = newData.toArray()
-    .map(row => Object.values(row).join(','))
-    .join('\n');
-
-  const headers = newData.schema.fields.map(f => f.name).join(',');
-  const csvWithHeaders = headers + '\n' + csv;
-
-  const blob = new Blob([csvWithHeaders], { type: 'text/csv' });
-  const url = URL.createObjectURL(blob);
-  const link = document.createElement('a');
-  link.href = url;
-  link.download = "mydata.csv";
-  link.click();
-  URL.revokeObjectURL(url);
+  const newData = await getTableDataForViewer(tableName, viewer, c, true);
+  // TODO 2024-12-16: make this go faster or non-blocking. worker-ify it probably? but maybe just async it.
+  const blob = arrowToCsv(newData)
+  downloadBlob(blob);
 };
